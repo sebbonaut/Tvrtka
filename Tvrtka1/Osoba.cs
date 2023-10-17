0665dfcd67f792bdee7901d0f9d173c0e153ba9a
sebas <sebas@DESKTOP-S92RB8H>
2023-10-17 11:06:56 +0200

    Add project files.

---
+++ Tvrtka1/Osoba.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tvrtka1
+{
+    internal abstract class Osoba
+    {
+        public string ime, prezime;
+        public abstract string Uloga {  get; }
+        public Osoba(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+        public Osoba() : this("John","Doe") { }
+        public virtual void Info()
+            => Console.WriteLine(ime + " " + prezime);
+    }
+
+    internal /* sealed */ class Zaposlenik : Osoba
+    {
+        public override string Uloga => "Zaposlenik";
+        public string titula = "prof.";
+        public /* sealed */ override void Info()
+        {
+            Console.Write(titula + " ");
+            base.Info();
+        }
+        public Zaposlenik(string ime, string prezime,
+            string titula) : base(ime, prezime)
+                => this.titula = titula;
+    }
+
+    internal sealed class Honorarni : Zaposlenik
+    {
+        public decimal honorar = 1000;
+        public override void Info()
+        {
+            Console.Write(honorar + "EUR ");
+            base.Info();
+        }
+        public Honorarni(string ime, string prezime,
+            string titula, decimal honorar)
+            : base(ime, prezime, titula)
+            => this.honorar = honorar;
+    }
+
+    internal class Student : Osoba
+    {
+        public override string Uloga => "Student";
+        public string jmbag = "1234567890";
+        public new void Info()
+            => Console.WriteLine($"{jmbag} {ime} {prezime}");
+    }
+}
---
+++ Tvrtka1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tvrtka1;
+
+Osoba a; // = new Osoba();
+Zaposlenik b = new Zaposlenik("a","b","p");
+//Console.WriteLine(b.ime);
+Student c = new Student();
+//Ispis((Student)a);
+
+Student s = new()
+{
+    ime = "Bob",
+    prezime = "Lee"
+};
+Osoba m = s;
+//Zaposlenik z = (Zaposlenik)m;
+//Zaposlenik? z = m as Zaposlenik;
+//z?.Info();
+if (m is Zaposlenik e)
+{
+    e.Info();
+}
+//else
+//    Console.WriteLine("Nista od konverzije!");
+
+//s.Info();
+//m.Info();
+m = b;
+//m.Info();
+Honorarni h = new Honorarni("Anne","Windsor","dr.",1000);
+//h.Info();
+IspisSvih(s, m, h, b);
+static void IspisSvih(params Osoba[] a)
+{
+    foreach (Osoba o in a)
+        o.Info();
+}
+//Osoba a1; // = new Osoba("Edgar","Poe");
+//Zaposlenik a2 = new Zaposlenik("Edgar","Poe","prof.");
+
+static void IspisUloge(Osoba a)
+{
+    Console.WriteLine(a.Uloga);
+}
+
+IspisUloge(h);
